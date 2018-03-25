c450f351213388ff3f598543a36f37fe1c1922b8
reluo <21404683@qq.com>
2018-03-26 00:29:11 +0800

    test web3

--- cloud/util/eth.js
+++ cloud/util/eth.js
@@ -5,6 +5,12 @@ var Web3 = require('web3');
 
 
 let abi = [{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"balanceOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[{"name":"initialSupply","type":"uint256"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"}]
+let testContract = {
+  address: '0x5A498f4109cba199173A8827729690c5258224f5' ,
+  privateKey: 'd606e705e3e89f193de90fb1b00e25ce4ee07eb89bb4a62c9f22f496bc89ce8a',
+  conAddress: '0x578BC65cac465A2cFD328bA620aFd34e617199e0'
+}
+
 let conAddress = '0x578BC65cac465A2cFD328bA620aFd34e617199e0'
 let conConAddress = '0x5A498f4109cba199173A8827729690c5258224f5'
 
@@ -19,6 +25,11 @@ async function testWeb3(){
   return balance
 }
 
+async function testAccounts(){
+  var web3 = new Web3(new Web3.providers.HttpProvider('https://ropsten.infura.io/'));
+  return await web3.eth.getAccounts()
+}
+
 async function createETHUser(){
   var web3 = new Web3(new Web3.providers.HttpProvider('https://ropsten.infura.io/'));
   let user = web3.eth.accounts.create()
@@ -27,20 +38,19 @@ async function createETHUser(){
 
 async function testCon(){
   var web3 = new Web3(new Web3.providers.HttpProvider('https://ropsten.infura.io/'));
-  var myCon = new web3.eth.Contract(abi,conAddress)
-  // let bal = await myCon.methods.balanceOf(conConAddress).call()
-  // return bal
-  try{
-    let bal = await myCon.methods.transfer(testUser.address, 100).send({from: conConAddress})
-    return bal
-  }catch(e){
-    // console.log('e=======================>',e)
-    return e.message
-  }
+  let wallet = web3.eth.accounts.wallet.add(testContract.privateKey)
+  // return wallet
+  web3.eth.defaultAccount = testContract.address
+  var myCon = new web3.eth.Contract(abi,testContract.conAddress)
+
+  let bal = await myCon.methods.transfer(testUser.address,555).send({from: wallet.})
+  return bal
+
 }
 
 export const ethFuncs = {
   testWeb3,
   createETHUser,
-  testCon
+  testCon,
+  testAccounts
 }
